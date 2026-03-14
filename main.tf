0e6e89dc3a7c12120ef7a9f89f2f9774e692cfab
lhywk <lhywkd22@gmail.com>
2026-03-15 01:31:20 +0900

    refactor: main.tf 파일을 리소스별로 분리
    유지보수성 향상을 위해 기능별로 파일 분할 (vpc, alb, rds 등)
    main.tf는 주석으로 구조만 남기고 리팩토링 완료

---
+++ main.tf
@@ -0,0 +1,20 @@
+/*
+  main.tf has been split into multiple focused files:
+    - providers.tf
+    - vpc.tf
+    - security_groups.tf
+    - iam.tf
+    - alb.tf
+    - ec2.tf
+    - rds.tf
+    - outputs.tf
+    - variables.tf
+    - terraform.tfvars
+    - .gitignore
+    - scripts/web_userdata.sh
+    - scripts/user_data.sh
+
+  These files contain the same resources previously defined here. Keep this file as a pointer only.
+*/
+
+// See the created files in the repository root and the scripts/ directory.
